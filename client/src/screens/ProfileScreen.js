44f76a1c8ac49d0b1e18d67c964d42798b130974
Mastagoon <iznilagiway@gmail.com>
2020-12-18 12:26:07 +0300

    user profile update

--- client/src/screens/ProfileScreen.js
+++ client/src/screens/ProfileScreen.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from "react-router-dom"
 import { Form, Button, Row, Col } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
 
 import Message from "../components/Message"
 import Loader from "../components/Loader"
-import { getUserDetails } from "../actions/userActions"
+import { getUserDetails, updateUserProfile } from "../actions/userActions"
 
 const ProfileScreen = ({location, history}) => {
     const [email,setEmail] = useState("")
@@ -21,6 +20,9 @@ const ProfileScreen = ({location, history}) => {
 
     const userDetails = useSelector(state => state.userDetails)
     const { loading, error, user } = userDetails
+
+    const userUpdateProfile = useSelector(state => state.userUpdateProfile)
+    const { success } = userUpdateProfile
  
     useEffect(() => {
         if(!userInfo) {
@@ -37,11 +39,7 @@ const ProfileScreen = ({location, history}) => {
 
     const submitHandler = (e) => {
         e.preventDefault()
-        if(password !== confirmPassword)
-            setMessage("Passwords do not match.")
-        else 
-            // dispatch(register(name,email,password))
-            return
+        dispatch(updateUserProfile({ id:user._id, name, email, password }))
     }
 
 
@@ -52,6 +50,7 @@ const ProfileScreen = ({location, history}) => {
                 <h2>User Profile</h2>
                 {message && <Message variant="danger">{message}</Message>}
                 {error && <Message variant="danger">{error}</Message>}
+                {success && <Message variant="success">Profile updated succesfully!</Message>}
                 {loading && <Loader />}
                 <Form onSubmit={submitHandler}>
                     <Form.Group controlId="name">
